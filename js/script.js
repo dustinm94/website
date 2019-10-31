cc0396a09bf50508ec47524631eedf4aae93fc15
dustin <dmcallister9405@gmail.com>
2019-10-30 21:45:09 -0400

    website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    stringsElement: null,
+    strings: ['Golang', 'Python', 'Java', 'Red Team', 'Machine Learning', 'Artifical Intelligence', 'Somewhere in the cloud'],
+    typeSpeed: 90,
+    backSpeed: 30,
+    smartBackspace: true,
+    loop: true
+  });
