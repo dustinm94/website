917074483cfeefeb7e52a3b4f25058599eedb2b7
dustin <dmcallister9405@gmail.com>
2019-10-31 20:08:56 -0400

    fixed spelling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#typed', {
     stringsElement: null,
-    strings: ['Golang', 'Python', 'Java', 'Red Team', 'Machine Learning', 'Artifical Intelligence', 'Somewhere in the cloud'],
+    strings: ['Golang', 'Python', 'Java', 'Red Team', 'Machine Learning', 'Artificial Intelligence', 'Somewhere in the cloud'],
     typeSpeed: 90,
     backSpeed: 30,
     smartBackspace: true,
